cb032cae309d76a9ecfcf47c02dfef76af568028
caezar47 <iss47@ya.ru>
2018-11-09 12:54:07 +0300

    v.8.11.09

--- src/block/site/default/slick/.document-ready.js
+++ src/block/site/default/slick/.document-ready.js
@@ -3,11 +3,11 @@ $(function() {
 	var header = $('[data-slider-slick="slick-header"]');	
 	var gallery = $('[data-slider-slick="slick-gallery"]');	
 	var CMS__TPL_PATH = '/wp-content/themes/azbn7theme';  
-	//var CMS__TPL_PATH = '/esbvolga';  
+	//var CMS__TPL_PATH = '/tpl';  
 	var CMS__TPL_PATH = '';  
 	//local
-	var prevArrow = '<button type="button" class="btn-nav__item  is--prev  slick-prev"><span class="sr-only">Предыдущий слайд</span><svg class="icon-svg icon-icon-prev" role="img"><use xlink:href="'+ CMS__TPL_PATH +'/img/svg/sprite.svg#icon-prev"></use></svg></button>';
-	var nextArrow = '<button type="button" class="btn-nav__item  is--next  slick-next"><span class="sr-only">Следующий слайд</span><svg class="icon-svg icon-icon-next" role="img"><use xlink:href="'+ CMS__TPL_PATH +'/img/svg/sprite.svg#icon-next"></use></svg></button>';	
+	var prevArrow = '<button type="button" class="slick-btn  is--prev"><span class="sr-only">Предыдущий слайд</span><svg class="icon-svg icon-icon-prev" role="img"><use xlink:href="'+ CMS__TPL_PATH +'/img/svg/sprite.svg#icon-prev"></use></svg></button>';
+	var nextArrow = '<button type="button" class="slick-btn  is--next"><span class="sr-only">Следующий слайд</span><svg class="icon-svg icon-icon-next" role="img"><use xlink:href="'+ CMS__TPL_PATH +'/img/svg/sprite.svg#icon-next"></use></svg></button>';	
 	
 	$('.slick-cloned .content-block__preview').removeAttr('data-fancybox');
 	
@@ -38,22 +38,6 @@ $(function() {
 				settings: {
 					slidesToShow: 3,
 					slidesToScroll: 3,
-				}
-		    },
-		    {
-				breakpoint: 1400,
-				settings: {
-					//dots: false,
-					slidesToShow: 2,
-					slidesToScroll: 2,
-				}
-		    },
-		    {
-				breakpoint: 1025,
-				settings: {					
-					arrows: false,
-					slidesToShow: 2,
-					slidesToScroll: 2,
 				}
 		    },
 		    {
