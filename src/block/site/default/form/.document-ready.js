3b0847367ef03f1d40b6c74504298cef3e052f20
caezar47 <iss47@ya.ru>
2018-11-09 15:37:41 +0300

    v.8.11.09

--- src/block/site/default/form/.document-ready.js
+++ src/block/site/default/form/.document-ready.js
@@ -1,5 +1,5 @@
 $(".form__control[type='tel']").mask("+7 (999) 999-99-99",{placeholder:"+7 (___) ___-__-__"});
-$(".form__block").validationEngine(
+$(".form__panel").validationEngine(
 	'attach', {
 		promptPosition : "bottomLeft",
 		//scrollOffset: 200,		
